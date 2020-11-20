dc36f50c72f90a3954bdd7d162e11491ddea1308
marcos pinardi <exegeses@gmail.com>
2020-11-20 10:40:42 -0300

    repaso

--- README.md
+++ README.md
@@ -9,4 +9,9 @@
 
 ## Definición: OOP vs Procedural
 
+> Cuando programamos, procedural, programamos funciones.
+
+> Generalmente tenemos separados los datos de los procedimientos.   
+> En cambio cuando programamos orientado a objetos, 
+> estos estan íntimamente relacionados en lo que conocemos como estado.   
 
--- clase1/repaso.php
+++ clase1/repaso.php
@@ -1,3 +1,29 @@
 <?php
 
-    
+    $x = 10;
+    const MONEDA = 'usd';
+    define('CURSO', 'PHP OOP');
+
+    function foo()
+    {
+        return CURSO;
+    }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= CURSO ?></title>
+</head>
+<body>
+<?php
+    echo foo();
+    echo '<br>';
+    //unset($x);
+    echo $x;
+
+?>
+
+
+</body>
+</html>
