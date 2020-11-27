8c3c1093d425ddd871d34b8d846410028f8b12d7
marcos pinardi <exegeses@gmail.com>
2020-11-27 11:23:07 -0300

    clase Conexion

--- agencia/clases/Conexion.php
+++ agencia/clases/Conexion.php
@@ -2,5 +2,19 @@
 
     class Conexion
     {
+        private static $link;
 
+        private function __construct()
+        {} // impedimos instanciar la clase
+
+        static function conectar(){
+            if( !isset( self::$link ) ){
+                self::$link = new PDO(
+                                    'mysql:host=localhost;dbname=agencia',
+                               'root',
+                                 'root'
+                                    );
+            }
+            return self::$link;
+        }
     }
