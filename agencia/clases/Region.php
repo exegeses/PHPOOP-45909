168f83c3960698c178807eaa6a8b07a2b088cb83
marcos pinardi <exegeses@gmail.com>
2020-11-27 12:25:30 -0300

    método listarRegiones()

--- agencia/clases/Region.php
+++ agencia/clases/Region.php
@@ -2,5 +2,49 @@
 
     class Region
     {
+        private $regID;
+        private $regNombre;
+
+        public function listarRegiones()
+        {
+            $link = Conexion::conectar();
+            $sql = "SELECT regID, regNombre
+                        FROM regiones";
+            $stmt = $link->prepare($sql);
+            $stmt->execute();
+
+            $regiones = $stmt->fetchAll();
+            return $regiones;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getRegID()
+        {
+            return $this->regID;
+        }
+        /**
+         * @param mixed $regID
+         */
+        public function setRegID($regID): void
+        {
+            $this->regID = $regID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getRegNombre()
+        {
+            return $this->regNombre;
+        }
+        /**
+         * @param mixed $regNombre
+         */
+        public function setRegNombre($regNombre): void
+        {
+            $this->regNombre = $regNombre;
+        }
 
     }
