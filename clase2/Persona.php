9f058824a65fa557bf6eba86bd96d4936c0701cc
marcos pinardi <exegeses@gmail.com>
2020-11-27 10:14:56 -0300

    ejemplo de método constructor

---
+++ clase2/Persona.php
@@ -0,0 +1,54 @@
+<?php
+
+    class Persona
+    {
+        private $nombre;
+        private $apellido;
+
+        public function __construct($nombre, $apellido)
+        {
+            $this->setNombre($nombre);
+            $this->setApellido($apellido);
+        }
+
+        public function verDatos()
+        {
+            $mensaje = 'Nombre: '.$this->getNombre().'<br>';
+            $mensaje .= 'Apellido: '.$this->getApellido().'<br>';
+
+            return $mensaje;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getNombre()
+        {
+            return $this->nombre;
+        }
+
+        /**
+         * @param mixed $nombre
+         */
+        public function setNombre($nombre): void
+        {
+            $this->nombre = $nombre;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getApellido()
+        {
+            return $this->apellido;
+        }
+
+        /**
+         * @param mixed $apellido
+         */
+        public function setApellido($apellido): void
+        {
+            $this->apellido = $apellido;
+        }
+
+    }
---
+++ clase2/vistaPersona.php
@@ -0,0 +1,21 @@
+<?php
+    require '../config/config.php';
+    require 'Persona.php';
+    // instanciamos objeto de tipo Persona
+    $Persona = new Persona('Morty', 'Smith');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>VistaPersona</h1>
+    <div class="objeto">
+        <?= $Persona->verDatos(); ?>
+    </div>
+
+</body>
+</html>
