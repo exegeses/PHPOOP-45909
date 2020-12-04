578ca5593d0cc06a4a8f5b72fe93128533b326ff
marcos pinardi <exegeses@gmail.com>
2020-12-04 10:08:49 -0300

    método listarDestinos()

--- agencia/clases/Destino.php
+++ agencia/clases/Destino.php
@@ -2,5 +2,160 @@
 
     class Destino
     {
+        private $destID;
+        private $destNombre;
+        private $regID;
+        static  $regNombre;
+        private $destPrecio;
+        private $destAsientos;
+        private $destDisponibles;
+        private $destActivo;
+
+    ##################################
+    ### CRUD de destinos
+
+        public function listarDestinos()
+        {
+            $link = Conexion::conectar();
+            $sql = "SELECT destID, destNombre,
+                           destinos.regID, regiones.regNombre
+                           destPrecio, 
+                           destAsientos, destDisponibles,
+                           destActivo
+                       FROM destinos, regiones
+                       WHERE destinos.regID = regiones.regID";
+            $stmt = $link->prepare($sql);
+            $stmt->execute();
+            $destinos = $stmt->fetchAll();
+            return $destinos;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestID()
+        {
+            return $this->destID;
+        }
+
+        /**
+         * @param mixed $destID
+         */
+        public function setDestID($destID): void
+        {
+            $this->destID = $destID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestNombre()
+        {
+            return $this->destNombre;
+        }
+
+        /**
+         * @param mixed $destNombre
+         */
+        public function setDestNombre($destNombre): void
+        {
+            $this->destNombre = $destNombre;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getRegID()
+        {
+            return $this->regID;
+        }
+
+        /**
+         * @param mixed $regID
+         */
+        public function setRegID($regID): void
+        {
+            $this->regID = $regID;
+        }
+
+        /**
+         * @return mixed
+         */
+        public static function getRegNombre()
+        {
+            return self::$regNombre;
+        }
+
+        /**
+         * @param mixed $regNombre
+         */
+        public static function setRegNombre($regNombre): void
+        {
+            self::$regNombre = $regNombre;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestPrecio()
+        {
+            return $this->destPrecio;
+        }
+
+        /**
+         * @param mixed $destPrecio
+         */
+        public function setDestPrecio($destPrecio): void
+        {
+            $this->destPrecio = $destPrecio;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestAsientos()
+        {
+            return $this->destAsientos;
+        }
+
+        /**
+         * @param mixed $destAsientos
+         */
+        public function setDestAsientos($destAsientos): void
+        {
+            $this->destAsientos = $destAsientos;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestDisponibles()
+        {
+            return $this->destDisponibles;
+        }
+
+        /**
+         * @param mixed $destDisponibles
+         */
+        public function setDestDisponibles($destDisponibles): void
+        {
+            $this->destDisponibles = $destDisponibles;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getDestActivo()
+        {
+            return $this->destActivo;
+        }
+
+        /**
+         * @param mixed $destActivo
+         */
+        public function setDestActivo($destActivo): void
+        {
+            $this->destActivo = $destActivo;
+        }
 
     }
